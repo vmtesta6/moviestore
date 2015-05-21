45a2b50bd23d0d8f5bcb4667f21b983b516e4a9a
Nik Hammer-Ellis <nikhammerellis@gmail.com>
2015-05-21 11:34:39 -0700

    shopping cart finished

---
+++ app/models/movie.rb
@@ -0,0 +1,17 @@
+class Movie < ActiveRecord::Base
+	def poster
+		"http://ia.media-imdb.com/images/M/#{poster_url}"
+	end
+
+	def imdb
+		"http://www.imdb.com/title/#{imdb_id}/"
+	end
+
+    def cart_action(current_user_id)
+      if $redis.sismember "cart#{current_user_id}", id
+        "Remove from"
+      else
+        "Add to"
+      end
+    end
+end
